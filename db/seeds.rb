0dbe8281e1336288695869ad0d1ffb30d572b2c5
OB <obuyuktezgel@gmail.com>
2021-04-28 01:13:20 +0200

    seeding part 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,40 +10,40 @@ require 'json'
 require 'open-uri'
 
 # ----------------------------------------------------------
-puts "1- Clearing the database..."
+# puts "1- Clearing the database..."
 
-  User.first.universities.destroy_all
+#   User.first.universities.destroy_all
 
-puts "Done."
-puts "2- Parsing university list..."
+# puts "Done."
+# puts "2- Parsing university list..."
 
-uni_url = 'https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json'
-uni_serialized = open(uni_url).read
-uni = JSON.parse(uni_serialized)
+# uni_url = 'https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json'
+# uni_serialized = open(uni_url).read
+# uni = JSON.parse(uni_serialized)
 
-puts "Done."
-puts "3- Creating universities..."
+# puts "Done."
+# puts "3- Creating universities..."
 
-  uni.each do |u|
-    University.create(name: u["name"],
-                      address: u["name"],
-                      url: u["web_pages"].first,
-                      country: u["country"],
-                      user: User.first)
-  end
+#   uni.each do |u|
+#     University.create(name: u["name"],
+#                       address: u["name"],
+#                       url: u["web_pages"].first,
+#                       country: u["country"],
+#                       user: User.first)
+#   end
 
-puts "#{University.count} universities have been added to the database!"
+# puts "#{University.count} universities have been added to the database!"
 
 # ----------------------------------------------------------
 
-# puts "Now making it fancier, please wait a little bit more."
+puts "Now making it fancier, please wait a little bit more."
 
-# University.all.each do |uni|
-#   if uni.name == uni.address
-#     uni.address = nil
-#     uni.save!
-#   end
-# end
+University.all.each do |uni|
+  if uni.name == uni.address
+    uni.address = nil
+    uni.save!
+  end
+end
 
 # ----------------------------------------------------------
 
